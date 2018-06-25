ed029903dbf93787f23eb35b2f9428c7c2e6ae4d
Marc Charbonneau <mtl2bk@gmail.com>
2018-06-24 19:38:53 -0700

    mail_room final

--- main.py
+++ main.py
@@ -1,41 +1,41 @@
 import os
 import base64
-
 from flask import Flask, render_template, request, redirect, url_for, session
-
 from model import Donation, Donor 
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return redirect(url_for('all'))
+
 
 @app.route('/donations/')
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
 
+
 @app.route('/donate/', methods=['GET', 'POST'])
 def donate():
     if request.method == 'POST':
-        donor = Donor.select().where(Donor.name == request.form['name']).get()
+        try:
+            donor = Donor.get(Donor.name == request.form['name'])
+            if donor:
+                Donation(value=request.form['donation'], donor=donor).save()
 
-        if donor:
-            donation = Donation(value=request.form['donation'], donor=request.form['name'])
-            donation.save()
-
-        else:
+        except Donor.DoesNotExist:
             donor = Donor(name=request.form['name'])
             donor.save()
-            donation = Donation(value=request.form['donation'], donor=request.form['name'])
-            donation.save()
+            Donation(value=request.form['donation'], donor=donor).save()
 
-        return render_template('donations.jinja2')
+        return redirect(url_for('all'))
 
-    return render_template('donate.jinja2')
+    else:
+        return render_template('donate.jinja2')
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
-
